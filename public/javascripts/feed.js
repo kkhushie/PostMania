a47b2960cd4d27138b24ffb11ae9dbce9867c10a
Khushi <Khushipal1470@gmail.com>
2024-06-16 19:33:34 +0530

    added feature to make links clickable

--- public/javascripts/feed.js
+++ public/javascripts/feed.js
@@ -28,13 +28,18 @@ function highlightMentions() {
 
     postContents.forEach(content => {
         // Regular expression to find mentions (words starting with @)
-        const regex = /@(\w+)/g;
+        const mentionRegex = /@(\w+)/g;
+        // Regular expression to find URLs
+        const urlRegex = /(https?:\/\/[^\s]+)/g;
+
         let html = content.innerHTML;
 
         // Replace mentions with anchor tags for linking to user profiles
-        html = html.replace(regex, '<a class="text-blue-500" href="/user/$1">@$1</a>');
+        html = html.replace(mentionRegex, '<a class="text-blue-500" href="/user/$1">@$1</a>');
+
+        // Replace URLs with anchor tags for clickable links
+        html = html.replace(urlRegex, '<a class="text-blue-500" href="$1" target="_blank" rel="noopener noreferrer">$1</a>');
+
         content.innerHTML = html;
     });
 }
-
-  
